a8e8f1c22472550a1d8c9986688e42cae3ecff4d
wanttobedone <19744784200@qq.com>
2026-01-21 21:08:28 +0800

    VINS测试

---
+++ shfiles/rspx4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 1. 启动 RealSense 相机 (使用 run_d455.launch)
+# 这里的 & 表示后台运行，sleep 5 是为了等相机初始化完再连飞控，防止USB冲突
+roslaunch ship_painter run_d455.launch & 
+sleep 5
+
+# 2. 启动 MAVROS (连接飞控)
+# 注意：/dev/ttyACM0 是飞控的串口号，如果不通请检查 ls /dev/tty*
+# 921600 是 Pixhawk 4/6C 等飞控的常用波特率，如果是旧版可能是 57600
+roslaunch mavros px4.launch fcu_url:=/dev/ttyACM0:921600 gcs_url:=udp://@192.168.1.100
+
+wait
